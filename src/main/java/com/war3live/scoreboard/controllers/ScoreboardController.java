f6970ce02d33b0be0fdcd78b14c49fc79c28410c
KorKux <cristhianeduardo03@hotmail.com>
2020-09-13 12:06:51 -0500

    feat: add controller components

--- src/main/java/com/war3live/scoreboard/controllers/ScoreboardController.java
+++ src/main/java/com/war3live/scoreboard/controllers/ScoreboardController.java
@@ -3,12 +3,114 @@ package com.war3live.scoreboard.controllers;
 import java.io.IOException;
 
 import com.war3live.scoreboard.main.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class ScoreboardController {
+    @FXML
+    private TextField fieldFolderPath;
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("scoreboard");
+    private Button btnFolderPath;
+
+    @FXML
+    private TextField fieldPlayer1Nickname;
+
+    @FXML
+    private TextField fieldPlayer1Score;
+
+    @FXML
+    private Button btnIncreasePlayer1Score;
+
+    @FXML
+    private Button btnReducePlayer1Score;
+
+    @FXML
+    private CheckBox checkAutoSave;
+
+    @FXML
+    private Button btnSwitchPlayers;
+
+    @FXML
+    private Button btnResetPlayers;
+
+    @FXML
+    private Button btnResetScore;
+
+    @FXML
+    private TextField fieldPlayer2Nickname;
+
+    @FXML
+    private TextField fieldPlayer2Score;
+
+    @FXML
+    private Button btnIncreasePlayer2Score;
+
+    @FXML
+    private Button btnReducePlayer2Score;
+
+    @FXML
+    private TextField fieldMapName;
+
+    @FXML
+    private TextField fieldGameLabel;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    void chooseFolder(ActionEvent event) {
+
     }
+
+    @FXML
+    void generateFields(ActionEvent event) {
+
+    }
+
+    @FXML
+    void increasePlayer1Score(ActionEvent event) {
+
+    }
+
+    @FXML
+    void increasePlayer2Score(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mapSuggestion(KeyEvent event) {
+
+    }
+
+    @FXML
+    void reducePlayer1Score(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reducePlayer2Score(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetPlayers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetScore(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchPlayers(ActionEvent event) {
+
+    }
+
+
 }
--- src/main/java/com/war3live/scoreboard/main/App.java
+++ src/main/java/com/war3live/scoreboard/main/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(loadFXML("scoreboard"));
         stage.setScene(scene);
         stage.show();
     }
